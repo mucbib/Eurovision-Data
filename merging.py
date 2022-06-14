763f85aaf800193c8f20e4074e361f13100c6ede
mucbib <105451683+mucbib@users.noreply.github.com>
2022-06-14 23:18:43 +0200

    Add files via upload
    making code more readable

--- merging.py
+++ merging.py
@@ -1,20 +1,24 @@
 import pandas as pd
 
-#finaldata = pd.read_csv('eurotable_final.csv', dtype={'Place': int})
+print("Merging all data...\n")
+
 finaldata = pd.read_csv('eurotable_final.csv')
-finaldata['Draw'] = finaldata.Draw.astype('Int64')
+finaldata['Order'] = finaldata.Order.astype('Int64')
 finaldata['Place'] = finaldata.Place.astype('Int64')
 finaldata['Points'] = finaldata.Points.astype('Int64')
 finaldata = finaldata.drop(labels="Unnamed: 0", axis=1)
 
-#print(finaldata)
 semidata = pd.read_csv('eurotable_semi.csv')
 semidata = semidata.drop(labels="Unnamed: 0", axis=1)
-semidata['SF_Draw'] = semidata.SF_Draw.astype('Int64')
+semidata['SF_Order'] = semidata.SF_Order.astype('Int64')
 semidata['SF_Place'] = semidata.SF_Place.astype('Int64')
 semidata['SF_Points'] = semidata.SF_Points.astype('Int64')
 
+#merging
 totaldata = finaldata.merge(semidata, how='outer', on=['Year', 'Country', 'Song', 'Artist', 'Language'])
+#save to CSV
 totaldata.to_csv('totaldata.csv')
+
+print("Viva la Diva !\n")
 print(totaldata)
 
