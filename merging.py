55e96c31c4d54939bcd663b81fb84002cb0e0a36
mucbib <105451683+mucbib@users.noreply.github.com>
2022-06-16 18:30:44 +0200

    Add files via upload
    solves issue 3 https://github.com/mucbib/Eurovision-Data/issues/3:
    removing duplicates from the merging of final and semi-final data. Resolved not in a very elegant way, but it works.

--- merging.py
+++ merging.py
@@ -15,10 +15,27 @@ semidata['SF_Place'] = semidata.SF_Place.astype('Int64')
 semidata['SF_Points'] = semidata.SF_Points.astype('Int64')
 
 #merging
-totaldata = finaldata.merge(semidata, how='outer', on=['Year', 'Country', 'Song', 'Artist', 'Language'])
+merge_subset = ['Year', 'Country', 'Song', 'Artist', 'Language']
+totaldata = finaldata.merge(semidata, how='outer', on=merge_subset)
+
+#deduplicating some duplicates from Wikipedia
+duplicate_subset = ['Year', 'Country', 'Artist', 'Song']
+##finding the duplicated rows and putting them apart in two dataframes
+duplicates1 = totaldata[totaldata.duplicated(duplicate_subset, keep='last')]
+duplicates2 = totaldata[totaldata.duplicated(duplicate_subset)]
+##merging the duplicate frames in one, dropping the Language-column in semifinals (duplicates2)
+duplicates1 = duplicates1.drop(['SF', 'SF_Order', 'SF_Points', 'SF_Place'], axis=1)
+duplicates2 = duplicates2.drop(['Language', 'Order', 'Place', 'Points'], axis=1)
+duplicatestotal = duplicates1.merge(duplicates2, how='outer', on=['Year', 'Country', 'Song', 'Artist'])
+#merging the cleaned-up rows everything with the big table and deduplicating, keeping the cleaned-up rows
+totaldata = totaldata.append(duplicatestotal)
+totaldata = totaldata.drop_duplicates(subset=duplicate_subset, keep='last')
+#making everything nice and tidy again
+totaldata = totaldata.sort_values(['Year', 'Order', 'SF', 'SF_Order'])
+
+
 #save to CSV
 totaldata.to_csv('totaldata.csv')
-
 print("Viva la Diva !\n")
 print(totaldata)
 
