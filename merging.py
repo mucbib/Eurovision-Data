98fd987637704bd9f44935af42cd0be916a7ba07
mucbib <105451683+mucbib@users.noreply.github.com>
2022-06-26 11:05:23 +0200

    small update

--- merging.py
+++ merging.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 print("Merging all data...\n")
 
-finaldata = pd.read_csv('eurotable_final.csv')
+finaldata = pd.read_csv('data/eurotable_final.csv')
 finaldata['Order'] = finaldata.Order.astype('Int64')
 finaldata['Place'] = finaldata.Place.astype('Int64')
 finaldata['Points'] = finaldata.Points.astype('Int64')
 finaldata = finaldata.drop(labels="Unnamed: 0", axis=1)
 
-semidata = pd.read_csv('eurotable_semi.csv')
+semidata = pd.read_csv('data/eurotable_semi.csv')
 semidata = semidata.drop(labels="Unnamed: 0", axis=1)
 semidata['SF_Order'] = semidata.SF_Order.astype('Int64')
 semidata['SF_Place'] = semidata.SF_Place.astype('Int64')
@@ -35,7 +35,7 @@ totaldata = totaldata.sort_values(['Year', 'Order', 'SF', 'SF_Order'])
 
 
 #save to CSV
-totaldata.to_csv('totaldata.csv')
+totaldata.to_csv('data/totaldata.csv')
 print("Viva la Diva !\n")
 print(totaldata)
 
