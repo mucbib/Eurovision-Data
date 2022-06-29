95d6160f3cb4398e0f960f0e095d64df50e96c63
mucbib <105451683+mucbib@users.noreply.github.com>
2022-06-29 19:09:36 +0200

    new function: user-defined start/end year

--- ff_eurofunctions.py
+++ ff_eurofunctions.py
@@ -1,6 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import datetime
+
+#define timeframe to scrape (interaction with user)
+def define_years(modus):
+	if modus == "semi":
+		beginning = 2004
+	else:
+		beginning = 1956
+	
+	yeartoday = datetime.date.today().year
+	#defining the timeframe and removing 2020 from the list
+	print(f"From which contests do you want to get data ?\nPlease enter a start year ({beginning}-today):\n")
+	startvalid = False
+	endvalid = False
+
+	while startvalid == False:
+		start = input().strip()
+		if start.isnumeric() == False:
+			print("Please enter a valid year !")
+			continue
+		start = int(start)
+		if not beginning <= start <= yeartoday:
+			print(f"Year must be between {beginning} and today !")
+			continue
+		startvalid = True
+
+	print("\nThank you ! Now please enter an end year (between your start year and today):\n")
+
+	while endvalid == False:
+		end = input().strip()
+		if end.isnumeric() == False:
+			print("Please enter a valid year !")
+			continue
+		end = int(end)
+		if not beginning <= end <= yeartoday:
+			print(f"Year must be between {beginning} and today !")
+			continue
+		if end < start:
+			print("Ehm...your end year should be higher than your start year.")
+			continue
+		endvalid = True
+		#end has to be augmented so that the range-function (later used) delivers the correct year
+		end = end + 1
+
+	print("\nMerci chérie !\n")
+
+	#end muss größer als start sein
+
+	years = [*range(start, end)]
+	if 2020 in years:
+		years.remove(2020)
+	
+	return years
+
 
 #Find section in Wikipedia (finals)
 def get_offset_final(wikiurl, year):
